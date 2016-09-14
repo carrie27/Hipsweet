843d0836fc7e2faf534a41cbb28b58a4907e7300
irina <carrie2703@yandex.ru>
2016-09-14 13:34:28 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@
 })();
 
 
+
 (function(){
 	$(function(){
 		$('#phone').mask('8(000)000-00-00');
@@ -47,7 +48,7 @@
 (function() {
 	$(function() {
 		$('.page-header__nav-list li').mouseover(function() {
-			$(this).children('.team__drop').slideDown('slow');
+			$(this).children('.team__drop').stop(true, true).slideDown('slow');
 			$(this).mouseleave(function() {
 				$(this).children('.team__drop').slideUp('slow');
 			});
@@ -55,6 +56,32 @@
 	});
 		
 })();
+
+
+(function() {
+	$(function() {
+		$('.page-header__nav-list li').mouseover(function() {
+			$(this).children('.sweet__drop-list').stop(true, true).slideDown('slow');
+			$(this).mouseleave(function() {
+				$(this).children('.sweet__drop-list').slideUp('slow');
+			});
+		});
+	});
+		
+})();
+
+(function() {
+	$(function() {
+		$('.page-header__nav-list li').mouseover(function() {
+			$(this).children('.company__drop-list').slideDown('slow');
+			$(this).mouseleave(function() {
+				$(this).children('.company__drop-list').slideUp('slow');
+			});
+		});
+	});
+		
+})();
+
 
 (function() {
 	$('.our-team__list-item').on('click', function(e){
@@ -80,3 +107,40 @@
 	});
 
 })();
+
+
+(function() {
+	ymaps.ready(init);
+    var myMap;
+
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [59.89768473690439,30.396409280273396],
+            zoom: 13,
+            controls: []
+        });
+
+        myPlacemark = new ymaps.Placemark([59.89768473690439,30.396409280273396], 
+    	{ hintContent: 'Санкт-Петербург',
+    	 balloonContent: 'Hipsweet' },
+    	{ 
+    	iconLayout: 'default#image',
+        iconImageHref: 'image/icons/map-check.png',
+        iconImageSize: [42, 59],
+        iconImageOffset: [-8, -50] 
+    });
+
+        myMap.geoObjects.add(myPlacemark);
+
+        myMap.behaviors
+    // Отключаем некоторые включенные по умолчанию поведения:
+    //  - drag - перемещение карты при нажатой левой кнопки мыши;
+    //  - rightMouseButtonMagnifier - увеличение области, выделенной
+    //    правой кнопкой мыши.
+    .disable(['drag', 'rightMouseButtonMagnifier', 'scrollZoom']);
+    
+    }
+
+    
+})();	
+
